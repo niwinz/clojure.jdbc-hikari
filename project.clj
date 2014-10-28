4758d6355251103db2d455f6e7a0476e2e534eb6
Andrey Antukh <niwi@niwi.be>
2014-10-28 10:40:31 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clojure.jdbc "0.3.0"]
                  [hikari-cp "0.8.0" :exclusions [com.zaxxer/HikariCP
                                                  org.clojure/clojure]]
                  [com.zaxxer/HikariCP "2.1.0"]]
-  :profiles {:dev {:dependencies [[com.h2database/h2 "1.3.176"]
-                                  [clojure.jdbc "0.3.0"]]}})
+  :profiles {:dev {:dependencies [[com.h2database/h2 "1.3.176"]]}})
